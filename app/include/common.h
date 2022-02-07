b72ea2afabe879a53a1c74b4b719ce00526f8715
0769 <xiongbinbin@mudu.tv>
2022-02-07 18:17:40 +0800

    create

---
+++ app/include/common.h
@@ -0,0 +1,23 @@
+#ifndef MDC_COMMON_H
+#define MDC_COMMON_H
+
+#include <iostream>
+#include <assert.h>
+#include <unistd.h>
+#include <string.h>
+
+
+#define     SUCCESS             (0)
+#define     FAILED              (-1)
+
+
+#define SAFE_FREE(x) do\
+{\
+	if(NULL != (x))\
+	{\
+		free(x);\
+		(x)=NULL;\
+	}\
+}while(0)
+
+#endif
---
+++ app/src/main.cpp
@@ -0,0 +1,12 @@
+#include "common.h"
+
+
+int main(int argc, char *argv[])
+{
+    while (true)
+    {
+        pause();
+    }
+
+    return 0;
+}
